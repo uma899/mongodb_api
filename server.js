bb89f6a0ecbc52592b75f566a4b59b11e4b353b1
uma899 <umamashesh006@gmail.com>
2025-01-08 00:14:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use("/record", records);
+app.use("/record", records);
 
-
-app.get((req, res) => {
-  res.json("Working");
-})
 
 // start the Express server
 app.listen(PORT, () => {
